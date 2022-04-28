a212107bdea1a25866125452ad242bba83c395b8
rahidhaque <ontirahid@yahoo.com>
2022-04-29 05:30:42 +0600

    Server Deployed

--- ___serverDeploy.js
+++ ___serverDeploy.js
@@ -1,6 +1,16 @@
 /****
+ * -------------------------------
+ * One Time For your computer
+ * -------------------------------
  * 1. Create heroku account
  * 2. Verify email
  * 3. Install heroku cli
  * 4. heroku login
+ * -----------------------------
+ * For each project one time
+ * -----------------------------
+ * 1. heroku create
+ * 2. make sure you git add . , git commit , git push
+ * 3. git push heroku main
+ * 4. 
  */
